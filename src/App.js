2c6d13350622529bc4898302dbfe1a6f3a59f7c9
khaoulaajroudi <khaoula.ajroudi@gmail.com>
2022-01-04 10:49:32 +0100

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    Person: {
+      fullName: "AJROUDI Khaoula",
+      bio: "I ... ",
+      imgSrc:
+        "https://cdn-icons.flaticon.com/png/512/2202/premium/2202112.png?token=exp=1641235818~hmac=d01491a4ea0eed7aabe0363056fbc8f9",
+      profession: "web devlopment",
+    },
+    show: true,
+  };
+
+  ShowPerson = () => {
+    this.setState({
+      ...this.state,
+      show: !this.state.show,
+    });
+  };
+
+  render() {
+    return (
+      <div className="header">
+        {this.state.show && (
+          <>
+            <img
+              src={this.state.Person.imgSrc}
+              alt="logo"
+              className="img"
+            ></img>
+            <h1>{this.state.Person.fullName}</h1>
+            <h1>{this.state.Person.bio}</h1>
+            <h1>{this.state.Person.profession}</h1>
+          </>
+        )}
+
+        <button onClick={this.ShowPerson} className="btn">
+          Show Détails
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
